e39103401b493a4d08a32bb883a9e3e968847f20
emrah1999 <emraheyubovemrah@gmail.com>
2025-03-04 15:16:55 +0400

    Header finish2

--- resources/views/web/includes/styles.blade.php
+++ resources/views/web/includes/styles.blade.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('web/css/library/bootstrap.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('web/css/library/owl.carousel.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{asset("frontend/css/sweetalert2.min.css")}}">
-<link rel="stylesheet" type="text/css" href="{{ asset('web/css/style.css?ver=7.2') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('web/css/style.css?ver=7.3') }}">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 <style>
     .language-col{
