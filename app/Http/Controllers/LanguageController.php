d875c94831e34f08718f2571296a1a1e938d45a6
aishalieva0 <aysalieva6@gmail.com>
2025-11-06 12:21:17 +0400

    lang switch

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Blog;
+use App\CorporativeLogistic;
+use App\InternationalDelivery;
 use App\Menu;
+use App\News;
+use App\Service;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
+use Schema;
 
 class LanguageController extends Controller
 {
@@ -31,6 +37,40 @@ class LanguageController extends Controller
     //     return redirect($newUrl);
     // }
 
+    // public function set_locale_language($locale)
+    // {
+    //     if (array_key_exists($locale, Config::get('languages'))) {
+    //         Session::put('applocale', $locale);
+    //         App::setLocale($locale);
+    //     }
+
+    //     $previousUrl = url()->previous();
+
+    //     $path = parse_url($previousUrl, PHP_URL_PATH);
+    //     $segments = explode('/', trim($path, '/'));
+
+
+    //     $oldLocale = $segments[0];
+    //     $slug = end($segments);
+
+    //     $item = Menu::where("slug_{$oldLocale}", $slug)->first();
+
+    //     if (!$item) {
+    //         $item = Blog::where("slug_{$oldLocale}", $slug)->first();
+    //     }
+
+    //     if ($item) {
+    //         $segments[0] = $locale;
+    //         $segments[array_key_last($segments)] = $item->{"slug_{$locale}"};
+    //     } else {
+    //         $segments[0] = $locale;
+    //     }
+
+    //     $newUrl = url(implode('/', $segments));
+
+    //     return redirect($newUrl);
+    // }
+
     public function set_locale_language($locale)
     {
         if (array_key_exists($locale, Config::get('languages'))) {
@@ -39,15 +79,30 @@ class LanguageController extends Controller
         }
 
         $previousUrl = url()->previous();
-
         $path = parse_url($previousUrl, PHP_URL_PATH);
         $segments = explode('/', trim($path, '/'));
-
 
         $oldLocale = $segments[0];
         $slug = end($segments);
 
-        $item = Menu::where("slug_{$oldLocale}", $slug)->first();
+        $models = [
+            Menu::class,
+            Blog::class,
+            News::class,
+            Service::class,
+            InternationalDelivery::class,
+            CorporativeLogistic::class,
+        ];
+
+        $item = null;
+
+        foreach ($models as $model) {
+            if (Schema::hasColumn((new $model)->getTable(), "slug_{$oldLocale}")) {
+                $item = $model::where("slug_{$oldLocale}", $slug)->first();
+                if ($item)
+                    break;
+            }
+        }
 
         if ($item) {
             $segments[0] = $locale;
@@ -56,8 +111,6 @@ class LanguageController extends Controller
             $segments[0] = $locale;
         }
 
-        $newUrl = url(implode('/', $segments));
-
-        return redirect($newUrl);
+        return redirect(url(implode('/', $segments)));
     }
 }
